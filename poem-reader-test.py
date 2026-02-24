59484e61d61878e44844ad5a9e7ff3e0eff06ec1
Ilya <ilryaz08@gmail.com>
2026-02-24 14:00:43 +0300

    Added .gitignore, splitted app in poems.txt (containing poems) and poem_reader_test.py (containing code)

--- poem-reader-test.py
+++ poem-reader-test.py
@@ -2,28 +2,13 @@ import sys
 from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget,
                                QHBoxLayout, QLabel, QComboBox)
 
-texts = {
-    "Deutsch": """Über allen Gipfeln
-Ist Ruh,
-In allen Wipfeln
-Spürest du
-Kaum einen Hauch;
-Die Vöglein schweigen im Walde,
-Warte nur, balde
-Ruhest du auch.
-
--Goethe""",
-    "Русский": """Горные вершины
-Спят во тьме ночной;
-Тихие долины
-Полны свежей мглой;
-Не пылит дорога,
-Не дрожат листы...
-Подожди немного,
-Отдохнешь и ты.
-
--М. Ю. Лермонтов"""
-}
+def load_poems_from_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        poems = content.strip().split("\n\n\n")
+        return poems[0], poems[1]
+    
+de_poem, ru_poem = load_poems_from_file(r"C:\Users\ilrya\Desktop\Python\tests\poem-reader-test\poems.txt")
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -34,13 +19,13 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         main_layout = QHBoxLayout(central_widget)
 
-        self.label = QLabel(texts['Deutsch'])
+        self.label = QLabel(de_poem)
         font = self.label.font()
         font.setPointSize(16)
         self.label.setFont(font)
 
         self.languages = QComboBox()
-        self.languages.addItems(texts.keys())
+        self.languages.addItems(["Deutsch", "Русский"])
 
         self.languages.currentTextChanged.connect(self.text_changed)
 
@@ -50,7 +35,10 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
     def text_changed(self, text):
-        self.label.setText(texts[text])
+        if text == "Deutsch":
+            self.label.setText(de_poem)
+        elif text == "Русский":
+            self.label.setText(ru_poem)
 
 
 app = QApplication(sys.argv)
